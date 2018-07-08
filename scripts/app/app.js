fc5d79a2df7faaf278ea1051028a7f53b69fc0ef
Michael Hemingway <mikemingway@gmail.com>
2018-07-08 15:58:35 -0400

    fixed small import problem

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,7 +1,9 @@
 import nav from './navigation.js'
-import UI from './ui.js'
+import ui from './ui.js'
 
 'use strict';
+
+document.body.parentNode.className = 'js'
 
 document.addEventListener('DOMContentLoaded', () => {
 	nav.onDOM()
--- scripts/app/ui.js
+++ scripts/app/ui.js
@@ -6,10 +6,10 @@ const anime = typeof window !== 'undefined' ? require('animejs') : null
 
 const UI = {
 	onDOM () {
-		// preloader animation
-		let preloader = document.getElementById('preloader').className = 'loaded'
-		// transitioner panels here w/ delay
-		window.setTimeout( function () { preloader.className = 'hidden' } , 1500)
+		let preloader = document.getElementById('preloader')
+
+		preloader.className = 'loaded'
+		window.setTimeout( () => { preloader.className = 'hidden' } , 1500)
 	}
 }
 
