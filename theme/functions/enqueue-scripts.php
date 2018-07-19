b193aa5a797453a8356392129635ab6af6862365
Michael Hemingway <mikemingway@gmail.com>
2018-07-19 00:27:37 -0400

    Integrated The Event Calendar plugin

--- theme/functions/enqueue-scripts.php
+++ theme/functions/enqueue-scripts.php
@@ -4,9 +4,10 @@ function site_scripts() {
   global $wp_styles;
 
   wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/js/app.min.js', array(), '', true );
+  wp_enqueue_script( 'twitter', get_template_directory_uri() . '/assets/js/twitter.min.js', array(), '1.0.0', true);
   wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
 
-  if ('feature' === get_post_type()) {
+  if ('feature' === get_post_type() || 'tribe_events' === get_post_type()) {
   	wp_enqueue_script( 'lightbox', get_template_directory_uri() . '/assets/js/lightbox.js', array(), '', true);
   	wp_enqueue_script( 'lazysizes', get_template_directory_uri() . '/assets/js/lazysizes.min.js', array(), '4.0.3', true);
   }
