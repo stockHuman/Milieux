7584aa7e4a8e03a8ae84e804c47f71e8c41b1334
Michael Hemingway <mikemingway@gmail.com>
2018-07-19 00:24:40 -0400

    Twitter nav logic

---
+++ scripts/social-feed/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+	// nav.onDOM()
+	// ui.onDOM()
+	let elem = document.createElement('div')
+	elem.id = 'twitter-toggle'
+	document.body.appendChild(elem)
+	elem.addEventListener('click', () => {
+		document.getElementById('twitter-panel').classList.toggle('--active')
+	})
+})
+
+// Customize twitter feed
+var hideTwitterAttempts = 0;
+function modifyTwitterStyles () {
+  setTimeout( function() {
+    var list = document.getElementsByTagName('iframe');
+    if (list.length ) {
+      Array.prototype.forEach.call( list, element => {
+      });
+    }
+    hideTwitterAttempts++;
+    if ( hideTwitterAttempts < 3 ) {
+     modifyTwitterStyles();
+    }
+  }, 1500);
+}
+modifyTwitterStyles();
