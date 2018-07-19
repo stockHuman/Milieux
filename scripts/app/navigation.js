256ab7047dce98a156bd38a96040ebbc58813f2b
Michael Hemingway <mikemingway@gmail.com>
2018-07-19 00:24:24 -0400

    Made nav reactive

--- scripts/app/navigation.js
+++ scripts/app/navigation.js
@@ -3,7 +3,20 @@
 */
 
 const Navigation = {
-	onDOM () {}
+	onDOM () {
+		const nav = document.getElementById('nav-main')
+		let navIsOpen = false
+
+		nav.addEventListener('click', () => {
+			if (navIsOpen) {
+				nav.classList.replace('nav-main--expanded', 'nav-main--collapsed')
+			} else {
+				nav.classList.replace('nav-main--collapsed', 'nav-main--expanded')
+			}
+			navIsOpen = !navIsOpen
+
+		})
+	}
 }
 
 export default Navigation
