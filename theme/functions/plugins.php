2ef9bc1c7cd0d6cf3415bbd45ddf1e4374f0a53b
Michael Hemingway <mikemingway@gmail.com>
2018-08-09 18:23:00 -0400

    ACF JSON integration and tribe events integration removal

--- theme/functions/plugins.php
+++ theme/functions/plugins.php
@@ -1,20 +1,6 @@
 <?php // Integrations with plugins
 
 /**
- * The Events Calendar (Milieux's preffered events management thing)
- *
- */
-
-// via https://theeventscalendar.com/knowledgebase/removing-the-add-on-upsell/
-define('TRIBE_HIDE_UPSELL', true);
-
-function replace_tribe_events_calendar_stylesheet() {
-   return; // silence is golden :P
-}
-add_filter('tribe_events_stylesheet_url', 'replace_tribe_events_calendar_stylesheet');
-
-
-/**
  * Return the featured image for an event (within the loop automatically will get event ID).
  *
  * Where possible, the image will be returned as a well formed <img> tag contained in a link
@@ -36,27 +22,37 @@ function milieux_event_featured_image ( $post_id = null, $size = 'full', $link =
 		$post_id = get_the_ID();
 	}
 
-	/**
-	 * Provides an opportunity to modify the featured image size.
-	 *
-	 * @param string $size
-	 * @param int    $post_id
-	 */
-	$size = apply_filters( 'tribe_event_featured_image_size', $size, $post_id );
-
 	$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), $size, false );
 
 	if ( is_array( $featured_image ) ) {
 		$featured_image = $featured_image[ 0 ];
 	}
 
-	return '<noscript><img= src="' . $featured_image .'"/></noscript>'
-				.'<div class="hero-container" style="padding-bottom: '
-				. _mlx_get_image_ratio_padding(get_post_thumbnail_id( $post_id ))
-				. '%"><img class="featured-image hero lazyload" src="'
-				. $featured_image
-				. '" srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="'
-				. ' data-srcset="' . wp_get_attachment_image_srcset(get_post_thumbnail_id( $post_id ))
-				. '" data-sizes="auto'
-				. '" alt="'. esc_html ( get_the_post_thumbnail_caption(get_post_thumbnail_id( $post_id )) ) . '"></div>';
+	return '<noscript><img src="' . $featured_image .'"/></noscript>'
+		.'<div class="hero-container" style="padding-bottom: '
+		. _mlx_get_image_ratio_padding(get_post_thumbnail_id( $post_id ))
+		. '%"><img class="featured-image hero lazyload" src="'
+		. $featured_image
+		. '" srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="'
+		. ' data-srcset="' . wp_get_attachment_image_srcset(get_post_thumbnail_id( $post_id ))
+		. '" data-sizes="auto'
+		. '" alt="'. esc_html ( get_the_post_thumbnail_caption(get_post_thumbnail_id( $post_id )) ) . '"></div>';
 }
+
+/**
+ * ACF save json folder
+ */
+function milieux_acf_json_save_point( $path ) {
+  return get_stylesheet_directory() . '/assets/acf-json';
+}
+add_filter( 'acf/settings/save_json', 'milieux_acf_json_save_point', 1 );
+
+/**
+ * ACF load json folder
+ */
+function milieux_acf_json_load_point( $paths ) {
+	unset($paths[0]);
+  $paths[] = get_stylesheet_directory() . '/assets/acf-json';
+  return $paths;
+}
+add_filter( 'acf/settings/load_json', 'milieux_acf_json_load_point', 1 );
