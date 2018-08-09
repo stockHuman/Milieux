fcb111fe1bba83aa63b0ef7fc09f7ccd98f6b9e6
Michael Hemingway <mikemingway@gmail.com>
2018-08-09 18:25:31 -0400

    make any item that has a featured image get lazysizes

--- theme/functions/enqueue-scripts.php
+++ theme/functions/enqueue-scripts.php
@@ -7,7 +7,7 @@ function site_scripts() {
   wp_enqueue_script( 'twitter', get_template_directory_uri() . '/assets/js/twitter.min.js', array(), '1.0.0', true);
   wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
 
-  if ('feature' === get_post_type() || 'tribe_events' === get_post_type()) {
+  if (has_post_thumbnail()) {
   	wp_enqueue_script( 'lightbox', get_template_directory_uri() . '/assets/js/lightbox.js', array(), '', true);
   	wp_enqueue_script( 'lazysizes', get_template_directory_uri() . '/assets/js/lazysizes.min.js', array(), '4.0.3', true);
   }
