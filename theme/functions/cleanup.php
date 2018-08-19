5d5e07b8209ec4b20d245d3700f8efc2ba312d04
Michael Hemingway <mikemingway@gmail.com>
2018-08-19 14:59:33 -0400

    Removed jQ migrate

--- theme/functions/cleanup.php
+++ theme/functions/cleanup.php
@@ -100,3 +100,12 @@ function milieux_get_the_author_posts_link() {
 	);
 	return $link;
 }
+
+
+function cedaro_dequeue_jquery_migrate( $scripts ) {
+	if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+		$jquery_dependencies = $scripts->registered['jquery']->deps;
+		$scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
+	}
+}
+add_action( 'wp_default_scripts', 'cedaro_dequeue_jquery_migrate' );
