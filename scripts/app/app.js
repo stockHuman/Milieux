352e093073d3e311674d344f7491c9ce317ff272
Michael Hemingway <mikemingway@gmail.com>
2018-08-19 14:55:26 -0400

    Event throtling code

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -5,7 +5,74 @@ import ui from './ui.js'
 
 document.body.parentNode.className = 'js'
 
+var scheduledAnimationFrame = false
+var requestResize = false
+var requestScroll = false
+
+const scroll = { lastY : null }
+const click = { last : null }
+const touch = { last : null }
+
 document.addEventListener('DOMContentLoaded', () => {
 	nav.onDOM()
 	ui.onDOM()
+
+	document.addEventListener('scroll', scrollHandler, {
+		// https://alligator.io/js/speed-up-scroll-events/
+		capture: true,
+		passive: true
+	})
+	window.addEventListener('resize', resizeHandler)
 })
+
+document.addEventListener('touchend', event => {
+  if (scheduledAnimationFrame) return;
+
+  scheduledAnimationFrame = true
+  touch.last = event.touches
+  requestAnimationFrame(update)
+})
+
+document.addEventListener('click', event => {
+  if (scheduledAnimationFrame) return;
+
+  scheduledAnimationFrame = true
+  click.last = {x: event.clientX, y: event.clientY}
+  requestAnimationFrame(update)
+})
+
+
+function scrollHandler (event) {
+	scroll.lastY = window.scrollY
+	if (scheduledAnimationFrame) return;
+
+	scheduledAnimationFrame = true
+	scroll.event = event
+	requestScroll = true
+	requestAnimationFrame(update)
+}
+
+
+function resizeHandler () {
+	scheduledAnimationFrame = true
+	requestResize = true
+	requestAnimationFrame(update)
+}
+
+
+function update () {
+	if (requestResize) {
+		requestResize = false
+		// place resize event fns here
+
+		nav.onResize()
+	}
+
+	if (requestScroll) {
+		requestScroll = false
+
+		// place scroll event fns here
+		nav.onScroll(scroll.lastY)
+	}
+	scheduledAnimationFrame = false
+}
