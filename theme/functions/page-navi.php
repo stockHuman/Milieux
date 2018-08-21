a107816ce547aad65d8684a0e5fdbbb31d922bbb
Michael Hemingway <mikemingway@gmail.com>
2018-08-21 19:51:42 -0400

    Page navigation styling

--- theme/functions/page-navi.php
+++ theme/functions/page-navi.php
@@ -7,37 +7,48 @@ function milieux_page_navi($before = '', $after = '') {
 	$paged = intval(get_query_var('paged'));
 	$numposts = $wp_query->found_posts;
 	$max_page = $wp_query->max_num_pages;
+
 	if ( $numposts <= $posts_per_page ) { return; }
+
 	if(empty($paged) || $paged == 0) {
 		$paged = 1;
 	}
+
 	$pages_to_show = 7;
 	$pages_to_show_minus_1 = $pages_to_show-1;
 	$half_page_start = floor($pages_to_show_minus_1/2);
 	$half_page_end = ceil($pages_to_show_minus_1/2);
 	$start_page = $paged - $half_page_start;
+
 	if($start_page <= 0) {
 		$start_page = 1;
 	}
+
 	$end_page = $paged + $half_page_end;
+
 	if(($end_page - $start_page) != $pages_to_show_minus_1) {
 		$end_page = $start_page + $pages_to_show_minus_1;
 	}
+
 	if($end_page > $max_page) {
 		$start_page = $max_page - $pages_to_show_minus_1;
 		$end_page = $max_page;
 	}
+
 	if($start_page <= 0) {
 		$start_page = 1;
 	}
+
 	echo $before.'<nav class="page-navigation"><ul class="pagination">'."";
 	if ($start_page >= 2 && $pages_to_show < $max_page) {
 		$first_page_text = __( 'First', 'milieux' );
 		echo '<li><a href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
 	}
+
 	echo '<li>';
 	previous_posts_link( __('Previous', 'milieux') );
 	echo '</li>';
+
 	for($i = $start_page; $i  <= $end_page; $i++) {
 		if($i == $paged) {
 			echo '<li class="current"> '.$i.' </li>';
@@ -45,12 +56,14 @@ function milieux_page_navi($before = '', $after = '') {
 			echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
 		}
 	}
+
 	echo '<li>';
 	next_posts_link( __('Next', 'milieux'), 0 );
 	echo '</li>';
+
 	if ($end_page < $max_page) {
 		$last_page_text = __( 'Last', 'milieux' );
 		echo '<li><a href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
 	}
 	echo '</ul></nav>'.$after."";
-} /* End page navi */
+}
