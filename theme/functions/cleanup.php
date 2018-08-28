62aece3e04c280081a85eb9c617e6cf21047c232
Michael Hemingway <mikemingway@gmail.com>
2018-08-27 22:58:34 -0400

    more complete dequeue of jQ migrate

--- theme/functions/cleanup.php
+++ theme/functions/cleanup.php
@@ -102,10 +102,8 @@ function milieux_get_the_author_posts_link() {
 }
 
 
-function cedaro_dequeue_jquery_migrate( $scripts ) {
-	if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
-		$jquery_dependencies = $scripts->registered['jquery']->deps;
-		$scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
-	}
+function dequeue_jquery_migrate( &$scripts ) {
+	$scripts->remove( 'jquery' );
+	$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.4.1' );
 }
-add_action( 'wp_default_scripts', 'cedaro_dequeue_jquery_migrate' );
+add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
