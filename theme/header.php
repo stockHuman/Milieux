897c5691bc103185905dd1d5758bb9f6d23459ba
Michael Hemingway <mikemingway@gmail.com>
2018-09-11 00:10:49 -0400

    new favicons

--- theme/header.php
+++ theme/header.php
@@ -5,35 +5,21 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name=viewport content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono|Roboto:100,900|Source+Sans+Pro:300,400" rel="stylesheet">
 
 	<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) {
 		$turi = get_template_directory_uri(); ?>
 		<!-- Icons & Favicons -->
-		<link rel="icon" href="<?= $turi ?>/assets/icons/favicon.ico">
-
-		<link rel="apple-touch-icon" sizes="57x57" href="<?= $turi ?>/assets/icons/apple-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="<?= $turi ?>/assets/icons/apple-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="<?= $turi ?>/assets/icons/apple-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?= $turi ?>/assets/icons/apple-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="<?= $turi ?>/assets/icons/apple-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="<?= $turi ?>/assets/icons/apple-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="<?= $turi ?>/assets/icons/apple-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="<?= $turi ?>/assets/icons/apple-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="<?= $turi ?>/assets/icons/apple-icon-180x180.png">
-		<link rel="icon" type="image/png" sizes="192x192" href="<?= $turi ?>/assets/icons/android-icon-192x192.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?= $turi ?>/assets/icons/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="<?= $turi ?>/assets/icons/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="96x96" href="<?= $turi ?>/assets/icons/favicon-96x96.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="<?= $turi ?>/assets/icons/favicon-16x16.png">
-
-		<meta name="msapplication-TileColor" content="#121212">
-		<meta name="msapplication-TileImage" content="<?= $turi ?>/assets/icons/ms-icon-144x144.png">
-
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?= $turi ?>/favicon.ico">
-		<![endif]-->
-
-		<meta name="theme-color" content="#121212">
+		<link rel="manifest" href="<?= $turi ?>/assets/icons/site.webmanifest">
+		<link rel="mask-icon" href="<?= $turi ?>/assets/icons/safari-pinned-tab.svg" color="#000000">
+		<link rel="shortcut icon" href="<?= $turi ?>/assets/icons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-config" content="<?= $turi ?>/assets/icons/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
   <?php } ?>
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
