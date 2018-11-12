a6fb999152157384f128e21ada83657faf04180b
Michael Hemingway <mikemingway@gmail.com>
2018-11-12 11:23:59 -0500

    New report template and style improvements

---
+++ theme/template-report.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Report */ ?>
+
+<?php get_header(); ?>
+
+	<div id="content" class="container">
+			<div id="inner-content" class="b-target">
+			<main id="main" class="report" role="main">
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<?php get_template_part( 'parts/loop', 'page' ); ?>
+
+				<?php endwhile; endif; ?>
+
+			</main> <!-- end #main -->
+		</div>
+	</div>
+
+<?php get_footer(); ?>
